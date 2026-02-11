c3c97e27a35ef8578f1ecc248e866c8b24f982e7
Firomsa Assefa <firomsassf@gmail.com>
2026-02-11 16:11:08 +0300

    feat: added cors origin configuration

--- Backend/src/main/java/com/firomsa/inventory/security/SecurityConfig.java
+++ Backend/src/main/java/com/firomsa/inventory/security/SecurityConfig.java
@@ -1,5 +1,8 @@
 package com.firomsa.inventory.security;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -8,33 +11,45 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import com.firomsa.inventory.config.AllowedOrigins;
 import com.firomsa.inventory.model.Roles;
+
+import lombok.RequiredArgsConstructor;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
         private final JWTSecurityFilter jwtSecurityFilter;
-
+        private final AllowedOrigins allowedOrigins;
         private final UnAuthorizedUserAuthenticationEntryPoint unAuthorizedUserAuthenticationEntryPoint;
-
-        public SecurityConfig(
-                        JWTSecurityFilter jwtSecurityFilter,
-                        UnAuthorizedUserAuthenticationEntryPoint unAuthorizedUserAuthenticationEntryPoint) {
-                this.jwtSecurityFilter = jwtSecurityFilter;
-
-                this.unAuthorizedUserAuthenticationEntryPoint = unAuthorizedUserAuthenticationEntryPoint;
+        
+        @Bean
+        CorsConfigurationSource corsConfigurationSource() {
+            CorsConfiguration configuration = new CorsConfiguration();
+            configuration.setAllowedOrigins(allowedOrigins.getOrigins());
+            configuration.setAllowedMethods(Arrays.asList("GET", "POST", "DELETE", "PUT"));
+            configuration.setAllowedHeaders(List.of("Authorization"));
+            UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+            source.registerCorsConfiguration("/**", configuration);
+            return source;
         }
-
+        
         @Bean
         public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity)
                         throws Exception {
                 return httpSecurity
                                 .csrf(csrf -> csrf.disable())
+                                .cors(Customizer.withDefaults())
                                 .authorizeHttpRequests(request -> request
                                                 .requestMatchers(
                                                                 "/api/v1/auth/**",
