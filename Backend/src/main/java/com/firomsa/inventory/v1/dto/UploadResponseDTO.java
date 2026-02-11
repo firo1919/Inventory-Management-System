c6a009109aa5bbdf9fcd8429107fbdaa242547b9
Firomsa Assefa <firomsassf@gmail.com>
2026-02-11 16:13:50 +0300

    added s3 support for storage and upload controller enppoints

---
+++ Backend/src/main/java/com/firomsa/inventory/v1/dto/UploadResponseDTO.java
@@ -0,0 +1,8 @@
+package com.firomsa.inventory.v1.dto;
+
+
+public record UploadResponseDTO(
+    String objectKey,
+    String uploadUrl,
+    String expiresIn) {
+}
---
+++ Backend/src/main/java/com/firomsa/inventory/v1/service/StorageService.java
@@ -0,0 +1,36 @@
+package com.firomsa.inventory.v1.service;
+
+import java.time.Duration;
+
+import org.springframework.stereotype.Service;
+
+import com.firomsa.inventory.v1.dto.UploadRequestDTO;
+import com.firomsa.inventory.v1.dto.UploadResponseDTO;
+
+import io.awspring.cloud.s3.S3Template;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class StorageService {
+
+    private final S3Template s3Template;
+    private final String bucketName = "inventory-management-system";
+    private final Integer expiresIn = 2;
+    
+    public UploadResponseDTO createUploadPresignTicket(UploadRequestDTO file) {
+        log.info("Generating presigned upload URL for file");
+        String key = System.currentTimeMillis() + "_" + file.getFilename();
+        return new UploadResponseDTO(key, s3Template.createSignedPutURL(bucketName, key, Duration.ofDays(expiresIn), null, file.getContentType()).toString(), expiresIn.toString());
+    }
+    
+    public boolean exists(String key) {
+        return s3Template.objectExists(bucketName, key);
+    }
+
+    public String getUrl(String key) {
+        return s3Template.createSignedGetURL(bucketName, key, Duration.ofDays(expiresIn)).toString();
+    }
+}
