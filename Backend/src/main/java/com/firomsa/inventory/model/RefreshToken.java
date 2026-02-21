6d0bf05e757209500c31cd89a4c98a7c0bcaebba
Firomsa Assefa <firomsassf@gmail.com>
2026-02-22 02:46:55 +0300

    refactor: update RefreshToken class to ensure token column is unique and defined as TEXT

--- Backend/src/main/java/com/firomsa/inventory/model/RefreshToken.java
+++ Backend/src/main/java/com/firomsa/inventory/model/RefreshToken.java
@@ -5,7 +5,7 @@ import java.util.UUID;
 
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.FetchType;
@@ -44,6 +44,7 @@ public class RefreshToken {
     private LocalDateTime createdAt;
 
     @NotNull
+    @Column(columnDefinition = "TEXT", unique = true)
     private String token;
 
 }
