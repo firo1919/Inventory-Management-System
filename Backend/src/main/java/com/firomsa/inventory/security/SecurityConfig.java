3880c50cfae86666b85ae376b03497be30ba6fd9
Firomsa Assefa <firomsassf@gmail.com>
2026-03-30 04:42:21 +0300

    feat: Restrict actuator endpoint access to admin role only

--- Backend/src/main/java/com/firomsa/inventory/security/SecurityConfig.java
+++ Backend/src/main/java/com/firomsa/inventory/security/SecurityConfig.java
@@ -101,7 +101,7 @@ public class SecurityConfig {
                         .requestMatchers("/api/v1/auth/**", "/docs", "/v3/api-docs",
                                 "/v3/api-docs/**", "/swagger-resources/**", "/swagger-ui.html",
                                 "/swagger-ui/**")
-                        .permitAll().requestMatchers("/api/v1/admin/**", "/actuator/**")
+                        .permitAll().requestMatchers("/api/v1/admin/**")
                         .hasAuthority("SCOPE_" + Roles.ADMIN.name())
                         .requestMatchers("/api/v1/employee/**")
                         .hasAuthority("SCOPE_" + Roles.EMPLOYEE.name()).anyRequest()
